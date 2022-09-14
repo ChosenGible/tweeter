7ff4d5c46a87a8a964dfd19299fcfe104fda5b66
ChosenGible <83.alex.72@gmail.com>
2022-09-14 16:24:53 -0600

    Updated the code to be MVP for login flow

---
+++ app/src/main/java/presenter/LoginPresenter.java
@@ -0,0 +1,76 @@
+package presenter;
+
+import android.widget.Toast;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+import model.service.UserService;
+
+public class LoginPresenter implements UserService.LoginObserver{
+    public LoginPresenter(LoginView view){
+        this.view = view;
+    }
+
+    private LoginView view;
+
+    //private methods
+    private String validateLogin(String alias, String password) {
+        if (alias.charAt(0) != '@') {
+            return "Alias must begin with @.";
+        }
+        if (alias.length() < 2) {
+            return "Alias must contain 1 or more characters after the @.";
+        }
+        if (password.length() == 0) {
+            return "Password cannot be empty.";
+        }
+        return null;
+    }
+
+    //methods that the view can call on the presenter
+    public void login(String username, String password){
+        String validateError = validateLogin(username, password);
+
+        if (validateError != null){
+            view.displayErrorMessage(validateError);
+
+        }
+        else {
+            view.clearErrorMessage();
+            view.displayInfoMessage("Logging In...");
+            new UserService().login(username, password, this);
+        }
+    }
+
+    //methods the presenter can call on the view
+    public interface LoginView {
+        void displayErrorMessage(String message);
+        void clearErrorMessage();
+
+        void displayInfoMessage(String message);
+        void clearInfoMessage();
+
+        void navigateToUser(User user);
+    }
+
+
+    //observer methods (something changed in the model)
+    @Override
+    public void handleLoginSuccess(User user, AuthToken token){
+        view.clearInfoMessage();
+        view.clearErrorMessage();
+
+        view.displayInfoMessage("Hello " + Cache.getInstance().getCurrUser().getName());
+
+        view.navigateToUser(user);
+    }
+    @Override
+    public void handleLoginFailed(String message){
+        view.displayInfoMessage("Failed to login: " + message);
+    }
+    @Override
+    public void handleLoginThrewException(Exception e){
+        view.displayInfoMessage("Failed to login because of exception: " + e.getMessage());
+    }
+}
